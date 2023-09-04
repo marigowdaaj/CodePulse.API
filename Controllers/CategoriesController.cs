8c9f77feed04ebe49cfc639964bddb38c17f54eb
subhranshu00 <subhranshu00@gmail.com>
2023-09-04 12:47:41 +0530

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,60 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        [HttpPost]
+        public async Task<ActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+            return Ok(response);
+        }
+
+        // Get: /api//categories
+        [HttpGet]
+        public async Task<ActionResult> GetAllCategories()
+        {
+            var categories = await categoryRepository.GetAllAsync();
+            
+            // map domain model to DTO
+
+            var response = new List<CategoryDto>();
+            foreach (var category in categories)
+            {
+                response.Add(new CategoryDto    
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle
+                });
+            }
+            return Ok(response);
+        }
+    }
+}   
